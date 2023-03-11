30af910b547cf46368b203a9c3978c662fcc73f0
Diego Brito <diegobritto50@gmail.com>
2023-03-11 00:50:07 -0300

    Projeto de estudo em JS

---
+++ js/anim.js
@@ -0,0 +1,50 @@
+iziToast.show({
+    title: 'Hey',
+    message: 'Como está o seu dia?',
+    messageColor: 'red',
+    position: 'center',
+    transitionLn: 'fadeInDown',
+    progressBar: 'false',
+    progressBarColor: 'coral',
+    timeOut: '105000'
+
+
+});
+
+let resp = document.getElementById('res')
+
+function acao1() {
+    resp.innerHTML = '<p> Dia 27 de Novembro, no dia do jogo da Libertadores do Flamengo x Palmeiras</p>'
+
+}
+
+function acao2() {
+    
+    resp.innerHTML = '<p>Em uma festa na piscina em Coelho da Rocha</p>'
+
+}
+
+function acao3() {
+    resp.innerHTML = '<p> Ver os muquiranas </p>'
+
+
+}
+
+function acao4() {
+    resp.innerHTML = '<p> Rosas vermelhas</p>'
+
+}
+
+function acao5() {
+    resp.innerHTML = '<p> Laranja</p>'
+
+}
+
+
+
+
+
+
+
+
+
---
+++ js/teste.js
@@ -0,0 +1,5 @@
+var x = 2
+var y = 1
+var result = x + y
+
+console.log(`${result} aqui está o resultado`)
