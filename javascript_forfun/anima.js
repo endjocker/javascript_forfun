fe621b160cc163328bbcdcbe689a56b36cd21368
Diego Brito <diegobritto50@gmail.com>
2023-03-11 16:08:22 -0300

    Atualização no CSS

---
+++ javascript_forfun/anima.js
@@ -0,0 +1,15 @@
+let resp = document.getElementById('resp')
+
+function clicksim() {
+
+        
+        resp.innerHTML = "<p>Você pode beber já</p>"
+
+}
+
+function clicknao() {
+
+        resp.innerHTML = "<p> Você é menor de idade, não poderá beber</p>"
+
+
+}
